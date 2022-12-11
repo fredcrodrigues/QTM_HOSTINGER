85eab68cf3d9c4dac67151a646116bab6c63f7d8
Fredson Costa Rodrigues <34144716+fredcrodrigues@users.noreply.github.com>
2022-12-11 17:07:50 -0300

    alterando seção de alto hiponse

--- resources/views/Site/partes_index/produto_audio.blade.php
+++ resources/views/Site/partes_index/produto_audio.blade.php
@@ -20,8 +20,8 @@
 
              <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 display-table md-margin-15px-top sm-no-margin-top sm-margin-ten-bottom xs-padding-five-lr xs-margin-ten-bottom wow fadeIn last-paragraph-no-margin">
                 <div class="display-table-cell vertical-align-middle padding-fourteen-right sm-no-padding-right sm-text-center">
-                    <h5 class="alt-font text-dark-gray">Escute um dos nossos áudios de Alto Hipnose</h5>
-                    <p class="width-85 md-width-100 xs-width-100 sm-margin-lr-auto text-medium-gray">A Auto Hipnose é um fenômeno simples que aplica a alteração legítima no sistema neurofisiológico e influencia no comportamento da pessoa hipnotizada</p>
+                    <h5 class="alt-font text-dark-gray">Escute um dos nossos áudios de Auto Hipnose</h5>
+                    <p class="width-85 md-width-100 xs-width-100 sm-margin-lr-auto text-medium-gray">A auto hipnose é um fenômeno simples que aplica a alteração legítima no sistema neurofisiológico e influencia no comportamento da pessoa hipnotizada</p>
                     <a  class="margin-35px-top btn btn-small btn-deep-pink sm-margin-30px-top" href="https://www.youtube.com/watch?v=UOG1oT7zgy8">ouvir</a>
                 </div>
             </div>
