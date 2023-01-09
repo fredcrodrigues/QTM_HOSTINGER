bc9214fd71144f054422324caceec0fd4fc85b94
Fredson Costa Rodrigues <34144716+fredcrodrigues@users.noreply.github.com>
2023-01-09 12:38:00 -0300

    alterando rederização de imagens

--- resources/views/Site/partes_index/parceiro.blade.php
+++ resources/views/Site/partes_index/parceiro.blade.php
@@ -4,7 +4,7 @@
             <div class="swiper-wrapper">
                 @foreach($parceiro as $parceiros)   
                 <!--'{{$parceiros->getFirstMediaUrl('parceiro-imagem')}}')-->
-                <div class="swiper-slide text-center" style="background-image: url('{{asset('storage/imagem_parceiros/parceiro-'.$parceiros->slug .'.jpg')}}'); background-repeat: no-repeat; background-size: contain; background-position: center center; width: 100%; height: 180px;"></div>
+                <div class="swiper-slide text-center" style="background-image: url('{{asset('site/images/parceiro-'.$parceiros->slug .'.jpg')}}'); background-repeat: no-repeat; background-size: contain; background-position: center center; width: 100%; height: 180px;"></div>
                 @endforeach
             </div>
         </div>
